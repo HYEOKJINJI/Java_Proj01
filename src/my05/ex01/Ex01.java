6859b45fff94a2fc68e36e081a4a7a428b561674
Hyokjin Ji <jhj930215@naver.com>
2022-11-17 22:51:55 +0900

    어우.. 모르겟다..

--- src/my05/ex01/Ex01.java
+++ src/my05/ex01/Ex01.java
@@ -1,15 +1,21 @@
 package my05.ex01;
 
+import java.util.Arrays;
+
 public class Ex01 {
 
 	public static void main(String[] args) {
-		String[][] str = new String[10][10];
-		for(int i=0; i < 4; i++) {
-			for (int j=0; j < 5; j++) {
-				
+		int sum = 0;
+		
+		for (int i = 1; i < args.length; i++) {
+			String a = String.valueOf(args[i]);
+			if (i%5==0) {
+				continue;
 			}
 		}
-
+		System.out.println(Arrays.toString(args));
+		
+		
 	}
 
 }
